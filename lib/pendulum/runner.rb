657265296abc8b800be758f93164720093eb10fa
monochromegane <dev.kuro.obi@gmail.com>
2016-03-12 21:23:01 +0900

    Implemented runner for CLI.

---
+++ lib/pendulum/runner.rb
@@ -0,0 +1,36 @@
+require 'optparse'
+
+module Pendulum
+  class Runner
+    def run(argv=ARGV)
+      api_key = nil
+      options = {file: 'Schedfile'}
+
+      opt.on('-f', '--file=FILE')  {|v| options[:file]    = v }
+      opt.on('-k', '--apikey=KEY') {|v| api_key           = v }
+
+      opt.parse!(argv) rescue return usage $!
+
+      begin
+        Client.new(api_key, options)
+      rescue
+        puts $!
+        return 1
+      end
+
+      return 0
+    end
+
+    private
+
+    def usage(err)
+      puts err if err
+      puts @opt.help
+      err ? 1 : 0
+    end
+
+    def opt
+      @opt ||= OptionParser.new
+    end
+  end
+end
