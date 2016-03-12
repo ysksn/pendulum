1592e52e63633b0aee704c562b311171f581e215
monochromegane <dev.kuro.obi@gmail.com>
2016-03-13 01:01:53 +0900

    Added with_options method.

--- lib/pendulum/dsl/output/base.rb
+++ lib/pendulum/dsl/output/base.rb
@@ -1,11 +1,22 @@
 module Pendulum::DSL::Output
   class Base
+    include Pendulum::DSL::Helper
+
     def initialize(&block)
       self.instance_eval(&block) if block_given?
     end
 
     def to_url
       raise NotImplementedError, "You must implement #{self.class}##{__method__}"
+    end
+
+    private
+
+    def with_options(url, *options)
+      params = (options || []).map do |option|
+        "#{option}=#{instance_variable_get("@#{option}")}"
+      end.join('&')
+      url + (params.empty? ? '' : "?#{params}")
     end
   end
 end
--- lib/pendulum/dsl/output/treasure_data.rb
+++ lib/pendulum/dsl/output/treasure_data.rb
@@ -1,13 +1,9 @@
 module Pendulum::DSL::Output
   class TreasureData < Base
-    include Pendulum::DSL::Helper
-
     define_setter :database, :table, :mode
 
     def to_url
-      url = "td://@/#{@database}/#{@table}"
-      url = "#{url}?mode=#{@mode}" if @mode
-      url
+      with_options("td://@/#{@database}/#{@table}", :mode)
     end
   end
 end
