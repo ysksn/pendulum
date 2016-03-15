453067b29f84e09bc73d666d1ff39492abfb623c
monochromegane <dev.kuro.obi@gmail.com>
2016-03-15 15:20:25 +0900

    Added dry-run and force options.

--- lib/pendulum/runner.rb
+++ lib/pendulum/runner.rb
@@ -1,33 +1,43 @@
 require 'optparse'
 
+DEFAULT_SCHEDFILE = 'Schedfile'
+
 module Pendulum
   class Runner
     def run(argv=ARGV)
       api_key = nil
-      mode    = :apply
+      mode    = nil
+      dry_run = false
+      force   = false
       options = {
-        file: 'Schedfile',
-        env:  :development
+        file:    DEFAULT_SCHEDFILE,
+        env:     :development,
       }
+      output  = DEFAULT_SCHEDFILE
 
-      opt.on('-k', '--apikey=KEY')  {|v| api_key = v }
-      opt.on('-f', '--file=FILE')   {|v| options[:file] = v }
+      opt.on('-k', '--apikey=KEY') {|v| api_key = v }
 
       # apply
+      opt.on('-a', '--apply')           {    mode   = :apply    }
       opt.on('-E', '--environment=ENV') {|v| options[:env]  = v }
+      opt.on('-f', '--file=FILE')       {|v| options[:file] = v }
+      opt.on('',   '--dry-run')         {    dry_run = true     }
+      opt.on('',   '--force')           {    force   = true     }
 
       # export
-      opt.on('-e', '--export')      { mode   = :export }
+      opt.on('-e', '--export')      {    mode   = :export }
+      opt.on('-o', '--output=FILE') {|v| output = v       }
 
       opt.parse!(argv) rescue return usage $!
+      return usage if (api_key.nil? || mode.nil?)
 
       begin
         client = Client.new(api_key, options)
         case mode
         when :apply
-          client.apply
+          client.apply(dry_run: dry_run, force: force)
         when :export
-          client.export(options[:file])
+          client.export(output)
         end
       rescue
         puts $!
@@ -39,7 +49,7 @@ module Pendulum
 
     private
 
-    def usage(err)
+    def usage(err=nil)
       puts err if err
       puts @opt.help
       err ? 1 : 0
